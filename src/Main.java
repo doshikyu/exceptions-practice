b742c045f1b639f35f6e044aef0feea26fd344ee
doshikyu <yu.dashik@gmail.com>
2024-08-26 14:27:26 +0900

    Completed Task
    - Refactoring (fixed bad exception usage)

--- src/Main.java
+++ src/Main.java
@@ -13,29 +13,26 @@ public class Main {
         System.out.print("Подтвердите пароль: ");
         String confirmPassword = myObj.nextLine();
 
-        checkAndPrintResult(login, password, confirmPassword);
-    }
-
-    static void checkAndPrintResult(String login, String password, String confirmPassword) {
         try {
-            if (!isValidField(login))
-                throw new WrongLoginException("Логин должен содержать только латинские буквы, цифры и символы подчеркивания, и не должен превышать 20 символов.");
-            if (!isValidField(password))
-                throw new WrongPasswordException("Пароль должен содержать только латинские буквы, цифры и символы подчеркивания, и не должен превышать 20 символов в длину.");
-            if (!password.equals(confirmPassword))
-                throw new WrongPasswordException("Пароль и пароль для подтверждения должны совпадать.");
-
+            checkAndPrintResult(login, password, confirmPassword);
             System.out.println("Вы успешно авторизировались!");
         } catch (WrongLoginException e) {
             System.out.println(e);
         } catch (WrongPasswordException e) {
             System.out.println(e);
         }
+    }
 
-//        } catch (WrongLoginException | WrongPasswordException e) {
-//            System.out.println(e);
-//        }
-
+    static void checkAndPrintResult(String login, String password, String confirmPassword) {
+        if (!isValidField(login)) {
+            throw new WrongLoginException("Логин должен содержать только латинские буквы, цифры и символы подчеркивания, и не должен превышать 20 символов.");
+        }
+        if (!isValidField(password)) {
+            throw new WrongPasswordException("Пароль должен содержать только латинские буквы, цифры и символы подчеркивания, и не должен превышать 20 символов в длину.");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароль и пароль для подтверждения должны совпадать.");
+        }
     }
 
     static boolean isValidCharacters(String checkString) {
